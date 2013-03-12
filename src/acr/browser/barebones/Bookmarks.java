70edfeddd52b41b1db0780e7ad0995216df64443
Aeefire <c.schartmueller@gmail.com>
2013-03-13 00:21:30 +0100

    Update Bookmarks.java
    style corrections.
    according to java style conventions, methods begin with a small character.
    Furthermore, instead of someBoolean == false, !someBoolean is used.

--- src/acr/browser/barebones/Bookmarks.java
+++ src/acr/browser/barebones/Bookmarks.java
@@ -6,20 +6,20 @@ import android.provider.Browser;
 import android.util.Log;
 
 public class Bookmarks extends Activity{
-	public void GetBookmarks(){
-    String[] projection = new String[] {
-   		Browser.BookmarkColumns.TITLE
-   		, Browser.BookmarkColumns.URL
-    };
-    Cursor mCur = getContentResolver().query(android.provider.Browser.BOOKMARKS_URI, projection, null, null, null);
-    mCur.moveToFirst();
-    int titleIdx = mCur.getColumnIndex(Browser.BookmarkColumns.TITLE);
-    int urlIdx = mCur.getColumnIndex(Browser.BookmarkColumns.URL);
-    String[] android = (Browser.HISTORY_PROJECTION);
-    while (mCur.isAfterLast() == false) {
-    	Log.i("Title", mCur.getString(titleIdx));
-    	Log.i("Title",mCur.getString(urlIdx));
-    	mCur.moveToNext();
+	public void getBookmarks(){
+    		String[] projection = new String[] {
+   				Browser.BookmarkColumns.TITLE, 
+   				Browser.BookmarkColumns.URL
+   				};
+    		Cursor mCur = getContentResolver().query(android.provider.Browser.BOOKMARKS_URI, projection, null, null, null);
+    		mCur.moveToFirst();
+    		int titleIdx = mCur.getColumnIndex(Browser.BookmarkColumns.TITLE);
+    		int urlIdx = mCur.getColumnIndex(Browser.BookmarkColumns.URL);
+    		String[] android = (Browser.HISTORY_PROJECTION);
+ 		while (!mCur.isAfterLast()) {
+    			Log.i("BookmarkTitle", mCur.getString(titleIdx));
+    			Log.i("BookmarkUrl",mCur.getString(urlIdx));
+    			mCur.moveToNext();
     }
 	}
 }
